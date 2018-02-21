ae0f2d2139d41db1cccb74a6abcc15101d1d8610
Bogdan Gradinariu <bogdan.gradinariu@gmail.com>
2018-02-21 16:01:18 +0200

    Pass all the arguments to the store
    Useful if you want to use this persistent store with some middleware.
    Example:
    ```
    const createPersistentStore = compose(
      persistState()
    )(createStore)
    
    export function createStoreWithState(state) {
      const newState = {
      // ...
      }
    
      return createPersistentStore(reducer, newState, myMiddlewares)
    }
    ```

--- src/persistState.js
+++ src/persistState.js
@@ -35,7 +35,7 @@ export default function persistState(paths, config) {
     deserialize
   } = cfg
 
-  return next => (reducer, initialState) => {
+  return next => (reducer, initialState, ...args) => {
     let persistedState
     let finalInitialState
 
@@ -46,7 +46,7 @@ export default function persistState(paths, config) {
       console.warn('Failed to retrieve initialize state from sessionStorage:', e)
     }
 
-    const store = next(reducer, finalInitialState)
+    const store = next(reducer, finalInitialState, ...args)
     const slicerFn = slicer(paths)
 
     store.subscribe(function () {
